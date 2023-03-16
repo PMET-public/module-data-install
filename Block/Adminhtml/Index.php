dfe5fbce010c0c02d98cd6cb9db763c443f40a8c
Jeff Britts <jbritts@adobe.com>
2023-03-16 15:12:37 -0500

    support for app builder ui

---
+++ Block/Adminhtml/Index.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright 2022 Adobe, Inc. All rights reserved.
+ * See LICENSE for license details.
+*/
+
+namespace MagentoEse\DataInstall\Block\Adminhtml;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+/**
+ * Block for services-id front end loader
+ *
+ * @api
+ */
+class Index extends Template
+{
+    /**
+     * Config Paths
+     * @var string
+     */
+    private const APP_URL_PATH = 'magentoese/datainstall/app_url';
+    private const CSS_URL_PATH = 'magentoese/datainstall/css_url';
+    private const BASE_URL_PATH = 'web/secure/base_url';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * 
+     * @param Context $context 
+     * @param ScopeConfigInterface $storeManager 
+     * @return void 
+     */
+    public function __construct(
+        Context $context,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        parent::__construct($context);
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * Returns config for app url
+     *
+     * @return string
+     */
+    public function getAppUrl(): string
+    {
+        return (string) $this->_scopeConfig->getValue(
+            self::APP_URL_PATH,
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT
+        );
+    }
+
+    /**
+     * Returns config for css url
+     *
+     * @return string
+     */
+    public function getCssUrl(): string
+    {
+        return (string) $this->_scopeConfig->getValue(
+            self::CSS_URL_PATH,
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT
+        );
+    }
+
+    
+    /**
+     * Returns Qraph!l BFF url
+     *
+     * @return string
+     */
+    public function getBaseUrl(): string
+    {
+        return $this->scopeConfig->getValue(
+            self::BASE_URL_PATH,
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT, 0);
+    }
+
+}
---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace MagentoEse\DataInstall\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Main page for Commerce Services Connector React application.
+ */
+class Index extends Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    //public const ADMIN_RESOURCE = 'Magento_ServicesId::configuration';
+
+    const MENU_ID = 'MagentoEse_DataInstall::datainstallui';
+
+    /**
+     * @var PageFactory
+     */
+    private $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/datainstallui_index_index.xml
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(static::MENU_ID);
+        $resultPage->getConfig()->getTitle()->prepend(__('Data Install UI (Beta)'));
+        return $resultPage;
+    }
+}
